a18b0aa9017b2f12548d28630b7ae630a4eda8cd
egarcia1997 <emmahacks@outlook.com>
2019-10-07 22:16:06 -0300

    agregada tabla de procesos

--- creacion.sql
+++ creacion.sql
@@ -1,8 +1,19 @@
+/* crea la base de datos */
 CREATE DATABASE Simulador;
 
+/* crea la tabla para guardar las particiones fijas */
 CREATE TABLE Simulador.ParticionesFijas (
 	idParticion INT PRIMARY KEY IDENTITY(0, 1),
 	dirInicio INT NOT NULL,
 	dirFin INT NOT NULL,
 	tamano INT NOT NULL,
+);
+
+/* crea la tabla para guardar los procesos */
+CREATE TABLE Simulador.Procesos (
+	idProceso INT PRIMARY KEY IDENTITY(0, 1),
+	tamano INT NOT NULL,
+	prioridad INT NOT NULL,
+	tiempoDeArribo INT NOT NULL,
+	cicloDeVida VARCHAR(10) NOT NULL,
 );
