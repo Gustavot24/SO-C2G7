33ac612f54bf57d7941f6fcc38c0ec75c7b18839
Mell_Rosas <melisa.rosas.93@gmail.com>
2019-10-09 14:36:25 -0300

    maquetado del front-end

--- Templates/Static/main.js
+++ Templates/Static/main.js
@@ -1,11 +1,7 @@
-function checked() {
-    var btnfix = document.getElementById ("prtfixed");
-    var btnvar = document.getElementById("prtvar");
-    if(btnfix.click === true){
-        document.getElementById ("wrtfit").disabled = true;
-    }else{
-        if(btnvar.click === true){
-            document.getElementById ("bstfit").disabled = true;
-        }
+function btnvar(){
+    var x = document.getElementById ('tblfix');
+    if(x.style.display === 'none'){
+        x.style.display= 'block';
     }
 }
+
