e240a1a340db084fefa5a64745ca7f78b355c21c
egarcia1997 <emmahacks@outlook.com>
2019-10-31 11:43:55 -0300

    ahora pude hacer andar la pagina accediendo desde localhost:8000. falta hacer que cargue el iconito y que se pueda probar en cualquier maquina

--- Templates/index.js
+++ Templates/index.js
@@ -1,7 +1,10 @@
 // script para arrancar la aplicacion
 // este coso se ejecuta con node
-// con info de https://stackoverflow.com/questions/4720343/loading-basic-html-in-node-js
+// con info de https://stackoverflow.com/questions/51201869/node-refused-to-apply-style-because-its-mime-type-text-html-is-not-a-support
+// y de https://medium.com/@asfo/desarrollando-una-sencilla-api-rest-con-nodejs-y-express-cab0813f7e4b
+// lectura adicional https://stackoverflow.com/questions/5924072/express-js-cant-get-my-static-files-why
 
+/*este coso no anda
 var http = require('http');
 var fs = require('fs');
 
@@ -14,4 +17,17 @@ fs.readFile('./Template2.html', function (err, html) {
         response.write(html);  
         response.end();  
     }).listen(8000);
+});*/
+
+var express = require('express');
+var app = new express();
+
+app.use('/Static', express.static('Static'))
+
+app.get('/', function(request, response){
+    response.sendFile('C:/Users/emmah/OneDrive/Facultad/sistemas operativos/repositorio local del tpi/Templates/Template2.html');
 });
+
+app.listen(8000, () => {
+    console.log("El servidor está inicializado en el puerto 8000");
+});   
