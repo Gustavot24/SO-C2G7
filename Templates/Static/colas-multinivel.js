eadc92bf47040250e3cc2c3a13b2fa7a8874eded
egarcia1997 <emmahacks@outlook.com>
2019-11-06 13:02:13 -0300

    intentando hacer los modales de colas (no estan terminados)

--- Templates/Static/colas-multinivel.js
+++ Templates/Static/colas-multinivel.js
@@ -1,20 +1,20 @@
-// carga una lista con los nombres de todas las listas de procesos guardadas
+// carga una lista con los nombres de todas las listas de colas guardadas
 // y abre el modal para poder seleccionar
 // con info de https://www.w3schools.com/jsref/met_select_remove.asp
 // y de https://www.w3schools.com/jsref/met_select_add.asp
-function modalCargarProcesos() {
-    var stringDeConsulta = "SELECT nombre FROM Simulador.dbo.Procesos;"; // crea el string de la consulta
+function modalCargarColas() {
+    var stringDeConsulta = "SELECT nombre FROM Simulador.dbo.Colas;"; // crea el string de la consulta
     var xhttp = new XMLHttpRequest(); // crea el objeto de la peticion ajax
     xhttp.onreadystatechange = function() { // esto se ejecuta cuando la peticion se complete
         if (this.readyState == 4 && this.status == 200) { // se ejecuta si se recibio la respuesta del servidor y no dio error
 			alert(this.responseText);
 			var listaDeNombres = JSON.parse(this.responseText).recordset;
             if (listaDeNombres.length == 0) { // si no devuelve ningun nombre no hay nada guardado, no abre el modal
-                mostrarMensaje("errorCargaDeTrabajo", "No hay ninguna lista de procesos guardada");
+                mostrarMensaje("errorColasMultinivel", "No hay ninguna lista de colas guardada");
             }
             else { // si devuelve algo hay algo guardado
-                $("#fromdblistproc").modal(); // muestra el modal
-                var comboBox = document.getElementById("selectProcesos"); // variable que apunta al select para elegir lista de procesos
+                $("#fromdbcolas").modal(); // muestra el modal
+                var comboBox = document.getElementById("selectColas"); // variable que apunta al select para elegir lista de colas
                 for (var i = comboBox.options.length - 1; i > 0; i--) { // elimina todas las opciones del combobox (menos la que dice "Selecciona")
                     comboBox.remove(i); // si no se hace del ultimo al primero falla
                 }
