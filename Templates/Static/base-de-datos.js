fa2bac0195c377a6f1a4b219a530be261b09068e
egarcia1997 <emmahacks@outlook.com>
2019-11-07 12:53:51 -0300

    casi casi terminada la funcion cargarParticiones()

--- Templates/Static/base-de-datos.js
+++ Templates/Static/base-de-datos.js
@@ -2,14 +2,6 @@
 // con info de https://medium.com/zero-equals-false/how-to-connect-node-js-app-with-sql-server-18a176afae46 (casi todo)
 // y de https://www.tutorialsteacher.com/nodejs/access-sql-server-in-nodejs (algunas cosas)
 
-// primero tenes que seguir todos los pasos del primer link (menos el que tiene el codigo js)
-// despues tenes que estar en la carpeta Templates
-// despues ejecutar npm init -y (no creo que sea necesario, ya hice y subi al git)
-// y despues npm install mssql (idem)
-
-// FALTA HACER:
-// QUE DE ALGUN MODO SE CREE LA DB Y LAS TABLAS ANTES DE ARRANCAR EL PROGRAMA
-
 // guarda la lista de particiones que esta en el html en la db
 // con info de https://stackoverflow.com/questions/13137597/how-to-get-element-inside-a-td-using-row-index-and-td-index
 function guardarParticiones(nombre) {
@@ -61,58 +53,41 @@ function guardarParticiones(nombre) {
 // con info de https://stackoverflow.com/questions/10911526/how-do-i-change-an-html-selected-option-using-javascript
 // y de https://www.w3schools.com/jsref/prop_range_value.asp
 function cargarParticiones(nombre) {
-    var tabla = document.getElementById("tabla-particiones"); // variable que apunta a la tabla de particiones
-    var stringDeConsulta = 'SELECT * FROM ParticionesFijas WHERE nombre = "' + nombre + '";'; // crea el string de la consulta
+    var tabla = document.getElementById("tabla-particiones").tBodies.item(0); // variable que apunta a la tabla de particiones
+    var stringDeConsulta = "SELECT * FROM Simulador.dbo.ParticionesFijas WHERE nombre = '" + nombre + "';"; // crea el string de la consulta
     var tablaParticiones;
-    sql.connect(config, function (err) { // ejecuta la conexion
-        if (err) { // si falla al conectarse tira el error en un alert
-            alert(err);
-        }
-        var request = new sql.Request(); // crea el objeto de la consulta
-        request.query(stringDeConsulta, function (err, resultado) { // ejecuta una consulta de ejemplo
-            if (err) { // si hay error en la consulta lo tira como un alert
-                alert(err);
+    var xhttp = new XMLHttpRequest(); // crea el objeto de la peticion ajax
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.responseText);
+            tablaParticiones = JSON.parse(this.responseText).recordset[0];
+            document.getElementById("mp").value = tablaParticiones.tamanoMemoria; // asigna el valor del tamaño de memoria al select
+            document.getElementById("myRange").value = tablaParticiones.porcentajeSO; // asigna el valor del porcentaje del SO al range
+            document.getElementById("demo").innerHTML = tablaParticiones.porcentajeSO; // asigna el valor del porcentaje del SO al label
+            if (tablaParticiones.algoritmo = "firstfit") {
+                document.getElementById("frtfit").checked = true;
             }
-            tablaParticiones = resultado.recordset[0]; // guarda el campo listado para cargar los datos en la pagina
-        });
-    });    
-    switch (tablaParticiones.tamanoMemoria) { // asigna el valor del tamaño de memoria al select
-        case "128":
-            document.getElementById("mp").value = "128";
-            break;
-        case "256":
-            document.getElementById("mp").value = "256";
-            break;
-        case "512":
-            document.getElementById("mp").value = "512";
-            break;
-        case "1024":
-            document.getElementById("mp").value = "1024";
-            break;
-        case "2048":
-            document.getElementById("mp").value = "2048";
-            break;
-    }
-    document.getElementById("myRange").value = tablaParticiones.porcentajeSO; // asigna el valor del porcentaje del SO al range
-    if (tablaParticiones.algoritmo = "firstfit") {
-        document.getElementById("frtfit").checked = true;
-    }
-    else {
-        document.getElementById("bstfit").checked = true;
-    }
-    var tablaConvertidaEnJSON = JSON.parse(tablaParticiones.listado); // convierte al string del json (directamente sacado del resultado de la consulta) en un array de objects
-    for (var i = 0; i < tablaConvertidaEnJSON.length; i++) { // itera  sobre el array de objects
-        var nuevaFila = tabla.insertRow(); //agrega una nueva fila a la tabla de particiones
-        var celdaIdParticion = nuevaFila.insertCell(0); // le va agregando las celdas a la fila esa
-        var celdaDirInicio = nuevaFila.insertCell(1);
-        var celdaDirFin = nuevaFila.insertCell(2);
-        var celdaTamano = nuevaFila.insertCell(3);
-        celdaIdParticion.innerHTML = tablaConvertidaEnJSON[i].idParticion; // le va cargando los datos a cada una de las celdas
-        celdaDirInicio.innerHTML = tablaConvertidaEnJSON[i].dirInicio;
-        celdaDirFin.innerHTML = tablaConvertidaEnJSON[i].dirFin;
-        celdaTamano.innerHTML = tablaConvertidaEnJSON[i].tamaño;
-    }
-    cargarParticionesVbles(); // ejecuta esta funcion de algoritmo.js para crear el object que tiene todos los datos de la pagina
+            else {
+                document.getElementById("bstfit").checked = true;
+            }
+            var tablaConvertidaEnJSON = JSON.parse(tablaParticiones.listado); // convierte al string del json (directamente sacado del resultado de la consulta) en un array de objects
+            for (var i = 0; i < tablaConvertidaEnJSON.length; i++) { // itera  sobre el array de objects
+                var nuevaFila = tabla.insertRow(); //agrega una nueva fila a la tabla de particiones
+                var celdaIdParticion = nuevaFila.insertCell(0); // le va agregando las celdas a la fila esa
+                var celdaDirInicio = nuevaFila.insertCell(1);
+                var celdaDirFin = nuevaFila.insertCell(2);
+                var celdaTamano = nuevaFila.insertCell(3);
+                celdaIdParticion.innerHTML = tablaConvertidaEnJSON[i].idParticion; // le va cargando los datos a cada una de las celdas
+                celdaDirInicio.innerHTML = tablaConvertidaEnJSON[i].dirInicio;
+                celdaDirFin.innerHTML = tablaConvertidaEnJSON[i].dirFin;
+                celdaTamano.innerHTML = tablaConvertidaEnJSON[i].tamaño;
+            }
+            cargarParticionesVbles(); // ejecuta esta funcion de algoritmo.js para crear el object que tiene todos los datos de la pagina        
+            mostrarMensaje("avisoCondicionesIniciales", "Se cargó la configuración de la memoria principal");
+        }
+    };
+    xhttp.open("POST", "/ejecutarConsulta?stringDeConsulta=" + stringDeConsulta, true);
+    xhttp.send();
 }
 
 // guarda la lista de procesos que esta en el html en la db
